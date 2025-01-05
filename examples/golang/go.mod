790d7d3589472ab4d98cda81ebeb00c4494d8b55
Nil Zork <ernest.micklei@gmail.com>
2025-01-05 22:27:14 +0100

    small refactoring

--- examples/golang/go.mod
+++ examples/golang/go.mod
@@ -6,7 +6,6 @@ go 1.23
 
 require (
 	github.com/emicklei/protocheck v0.0.0-20241212220435-83bbc3a88fb5
-	github.com/golang/protobuf v1.5.4
 	github.com/google/cel-go v0.22.1
 	google.golang.org/protobuf v1.36.1
 )
@@ -15,7 +14,7 @@ require (
 	cel.dev/expr v0.19.1 // indirect
 	github.com/antlr4-go/antlr/v4 v4.13.1 // indirect
 	github.com/stoewer/go-strcase v1.3.0 // indirect
-	golang.org/x/exp v0.0.0-20241217172543-b2144cdd0a67 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20241223144023-3abc09e42ca8 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20241223144023-3abc09e42ca8 // indirect
+	golang.org/x/exp v0.0.0-20250103183323-7d7fa50e5329 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250102185135-69823020774d // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250102185135-69823020774d // indirect
 )
--- examples/golang/person.check.go
+++ examples/golang/person.check.go
@@ -34,10 +34,11 @@ func file_health_check_proto_init() error {
 	messageCheckers := []protocheck.Checker{}
 	fieldCheckers := []protocheck.Checker{}
 	{ // Weight
-		if prg, err := protocheck.MakeProgram(env, `this.weight > 0`); err != nil {
+		expr := `this.weight > 0`
+		if prg, err := protocheck.MakeProgram(env, expr); err != nil {
 			return fmt.Errorf("protocheck.MakeProgram failed: %w", err)
 		} else {
-			ch := protocheck.NewChecker("", "weight in kg must be positive", `this.weight > 0`, "Weight", false, prg)
+			ch := protocheck.NewChecker("", "weight in kg must be positive", expr, "Weight", false, prg)
 			fieldCheckers = append(fieldCheckers, ch)
 		}
 	}
@@ -80,42 +81,47 @@ func file_person_check_proto_init() error {
 	}
 	fieldCheckers := []protocheck.Checker{}
 	{ // Name
-		if prg, err := protocheck.MakeProgram(env, `size(this.name) > 1`); err != nil {
+		expr := `size(this.name) > 1`
+		if prg, err := protocheck.MakeProgram(env, expr); err != nil {
 			return fmt.Errorf("protocheck.MakeProgram failed: %w", err)
 		} else {
-			ch := protocheck.NewChecker("", "name must be longer than 1", `size(this.name) > 1`, "Name", false, prg)
+			ch := protocheck.NewChecker("", "name must be longer than 1", expr, "Name", false, prg)
 			fieldCheckers = append(fieldCheckers, ch)
 		}
 	}
 	{ // MiddleName
-		if prg, err := protocheck.MakeProgram(env, `size(this.middle_name) > 0`); err != nil {
+		expr := `size(this.middle_name) > 0`
+		if prg, err := protocheck.MakeProgram(env, expr); err != nil {
 			return fmt.Errorf("protocheck.MakeProgram failed: %w", err)
 		} else {
-			ch := protocheck.NewChecker("", "middle name (if set) cannot be empty", `size(this.middle_name) > 0`, "MiddleName", true, prg)
+			ch := protocheck.NewChecker("", "middle name (if set) cannot be empty", expr, "MiddleName", true, prg)
 			fieldCheckers = append(fieldCheckers, ch)
 		}
 	}
 	{ // Surname
-		if prg, err := protocheck.MakeProgram(env, `size(this.surname) > 1`); err != nil {
+		expr := `size(this.surname) > 1`
+		if prg, err := protocheck.MakeProgram(env, expr); err != nil {
 			return fmt.Errorf("protocheck.MakeProgram failed: %w", err)
 		} else {
-			ch := protocheck.NewChecker("", "surname must be longer than 1", `size(this.surname) > 1`, "Surname", false, prg)
+			ch := protocheck.NewChecker("", "surname must be longer than 1", expr, "Surname", false, prg)
 			fieldCheckers = append(fieldCheckers, ch)
 		}
 	}
 	{ // BirthDate
-		if prg, err := protocheck.MakeProgram(env, `this.birth_date.getFullYear() > 2000`); err != nil {
+		expr := `this.birth_date.getFullYear() > 2000`
+		if prg, err := protocheck.MakeProgram(env, expr); err != nil {
 			return fmt.Errorf("protocheck.MakeProgram failed: %w", err)
 		} else {
-			ch := protocheck.NewChecker("check_birth_date", "[this.birth_date.getFullYear() > 2000] is false", `this.birth_date.getFullYear() > 2000`, "BirthDate", false, prg)
+			ch := protocheck.NewChecker("check_birth_date", "[this.birth_date.getFullYear() > 2000] is false", expr, "BirthDate", false, prg)
 			fieldCheckers = append(fieldCheckers, ch)
 		}
 	}
 	{ // Email
-		if prg, err := protocheck.MakeProgram(env, `this.email.matches('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')`); err != nil {
+		expr := `this.email.matches('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')`
+		if prg, err := protocheck.MakeProgram(env, expr); err != nil {
 			return fmt.Errorf("protocheck.MakeProgram failed: %w", err)
 		} else {
-			ch := protocheck.NewChecker("email", "email is not valid", `this.email.matches('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')`, "Email", false, prg)
+			ch := protocheck.NewChecker("email", "email is not valid", expr, "Email", false, prg)
 			ch = ch.WithEnabledFunc(func(x any) bool {
 				if x == nil {
 					return false
@@ -128,10 +134,11 @@ func file_person_check_proto_init() error {
 		}
 	}
 	{ // Phone
-		if prg, err := protocheck.MakeProgram(env, `this.phone.matches('^[0-9]{3}-[0-9]{3}-[0-9]{4}$')`); err != nil {
+		expr := `this.phone.matches('^[0-9]{3}-[0-9]{3}-[0-9]{4}$')`
+		if prg, err := protocheck.MakeProgram(env, expr); err != nil {
 			return fmt.Errorf("protocheck.MakeProgram failed: %w", err)
 		} else {
-			ch := protocheck.NewChecker("", "phone is not valid", `this.phone.matches('^[0-9]{3}-[0-9]{3}-[0-9]{4}$')`, "Phone", false, prg)
+			ch := protocheck.NewChecker("", "phone is not valid", expr, "Phone", false, prg)
 			ch = ch.WithEnabledFunc(func(x any) bool {
 				if x == nil {
 					return false
@@ -144,34 +151,38 @@ func file_person_check_proto_init() error {
 		}
 	}
 	{ // Nicknames
-		if prg, err := protocheck.MakeProgram(env, `size(this.nicknames) > 0`); err != nil {
+		expr := `size(this.nicknames) > 0`
+		if prg, err := protocheck.MakeProgram(env, expr); err != nil {
 			return fmt.Errorf("protocheck.MakeProgram failed: %w", err)
 		} else {
-			ch := protocheck.NewChecker("", "at least one nickname is required", `size(this.nicknames) > 0`, "Nicknames", false, prg)
+			ch := protocheck.NewChecker("", "at least one nickname is required", expr, "Nicknames", false, prg)
 			fieldCheckers = append(fieldCheckers, ch)
 		}
 	}
 	{ // Pets
-		if prg, err := protocheck.MakeProgram(env, `size(this.pets) > 0`); err != nil {
+		expr := `size(this.pets) > 0`
+		if prg, err := protocheck.MakeProgram(env, expr); err != nil {
 			return fmt.Errorf("protocheck.MakeProgram failed: %w", err)
 		} else {
-			ch := protocheck.NewChecker("", "at least one Pet is required", `size(this.pets) > 0`, "Pets", false, prg)
+			ch := protocheck.NewChecker("", "at least one Pet is required", expr, "Pets", false, prg)
 			fieldCheckers = append(fieldCheckers, ch)
 		}
 	}
 	{ // Attributes
-		if prg, err := protocheck.MakeProgram(env, `size(this.attributes) > 0`); err != nil {
+		expr := `size(this.attributes) > 0`
+		if prg, err := protocheck.MakeProgram(env, expr); err != nil {
 			return fmt.Errorf("protocheck.MakeProgram failed: %w", err)
 		} else {
-			ch := protocheck.NewChecker("", "at least one attribute is required", `size(this.attributes) > 0`, "Attributes", false, prg)
+			ch := protocheck.NewChecker("", "at least one attribute is required", expr, "Attributes", false, prg)
 			fieldCheckers = append(fieldCheckers, ch)
 		}
 	}
 	{ // Favorites
-		if prg, err := protocheck.MakeProgram(env, `size(this.favorites) > 0`); err != nil {
+		expr := `size(this.favorites) > 0`
+		if prg, err := protocheck.MakeProgram(env, expr); err != nil {
 			return fmt.Errorf("protocheck.MakeProgram failed: %w", err)
 		} else {
-			ch := protocheck.NewChecker("", "at least one favorite is required", `size(this.favorites) > 0`, "Favorites", false, prg)
+			ch := protocheck.NewChecker("", "at least one favorite is required", expr, "Favorites", false, prg)
 			fieldCheckers = append(fieldCheckers, ch)
 		}
 	}
@@ -191,13 +202,13 @@ func (x *Person) Validate() protocheck.ValidationError {
 		}
 	})
 	ve := personValidator.Validate(x)
-	for key, m := range x.GetPets() { // Pets
-		for _, nve := range m.Validate() {
+	for key, msg := range x.GetPets() { // Pets
+		for _, nve := range msg.Validate() {
 			ve = append(ve, nve.WithParentField("Pets", key))
 		}
 	}
-	for key, m := range x.GetFavorites() { // Favorites
-		for _, nve := range m.Validate() {
+	for key, msg := range x.GetFavorites() { // Favorites
+		for _, nve := range msg.Validate() {
 			ve = append(ve, nve.WithParentField("Favorites", key))
 		}
 	}
@@ -216,18 +227,20 @@ func file_pet_check_proto_init() error {
 	messageCheckers := []protocheck.Checker{}
 	fieldCheckers := []protocheck.Checker{}
 	{ // Kind
-		if prg, err := protocheck.MakeProgram(env, `this.kind == 'cat' || this.kind == 'dog'`); err != nil {
+		expr := `this.kind == 'cat' || this.kind == 'dog'`
+		if prg, err := protocheck.MakeProgram(env, expr); err != nil {
 			return fmt.Errorf("protocheck.MakeProgram failed: %w", err)
 		} else {
-			ch := protocheck.NewChecker("pet1", "only dog or cat is allowed", `this.kind == 'cat' || this.kind == 'dog'`, "Kind", false, prg)
+			ch := protocheck.NewChecker("pet1", "only dog or cat is allowed", expr, "Kind", false, prg)
 			fieldCheckers = append(fieldCheckers, ch)
 		}
 	}
 	{ // Name
-		if prg, err := protocheck.MakeProgram(env, `size(this.name) > 0`); err != nil {
+		expr := `size(this.name) > 0`
+		if prg, err := protocheck.MakeProgram(env, expr); err != nil {
 			return fmt.Errorf("protocheck.MakeProgram failed: %w", err)
 		} else {
-			ch := protocheck.NewChecker("", "name cannot be empty", `size(this.name) > 0`, "Name", false, prg)
+			ch := protocheck.NewChecker("", "name cannot be empty", expr, "Name", false, prg)
 			fieldCheckers = append(fieldCheckers, ch)
 		}
 	}
